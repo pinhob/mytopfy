701b5927175ab29655084cedb62c48815d9833cc
Bruno Pinho <brunopinho@outlook.com>
2022-04-04 11:14:28 -0300

    feat: create getAccessToken helper function

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import { accessToken } from './helpers/handleTokens';
 
 function App() {
+  const [token, getToken] = useState(null);
+
+
+  useEffect(() => {
+    getToken(accessToken);
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="http://localhost:8888/login"
-        >
-          Log in to Spotify
-        </a>
+        {
+          token
+            ? <p>Logged in</p>
+            : <a
+              className="App-link"
+              href="http://localhost:8888/login"
+            >
+              Log in to Spotify
+            </a>
+        }
       </header>
     </div>
   );
