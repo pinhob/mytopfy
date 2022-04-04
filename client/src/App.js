34817026464ae503fc6bd0437253fa8763d366de
Bruno Pinho <brunopinho@outlook.com>
2022-04-04 10:37:39 -0300

    feat: create react app

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="http://localhost:8888/login"
+        >
+          Log in to Spotify
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
