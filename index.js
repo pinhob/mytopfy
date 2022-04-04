63fc58f7531f3158abd06a648569e2fb53672740
Bruno Pinho <brunopinho@outlook.com>
2022-04-03 23:34:16 -0300

    feat: create initial get /callback request

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
+const { default: axios } = require('axios');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
+const FRONTEND_URI = process.env.FRONTEND_URI;
 
 const STATE_KEY = 'spotify_auth_state';
 
@@ -19,6 +22,8 @@ const generateRandomString = (length) => {
 
   return randomString;
 }
+
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -42,6 +47,50 @@ app.get('/login', (_req, res) => {
   }).toString();
 
   res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
+
+app.get('/callback', async (req, res) => {
+  try {
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+    const storedState = req.cookies ? req.cookies[STATE_KEY] : null;
+
+    if (state === null || state !== storedState) return res.redirect('/error=state_mismatch');
+
+    res.clearCookie(STATE_KEY);
+
+    const authOptions = {
+      method: 'post',
+      url: 'https://accounts.spotify.com/api/token',
+      data: new URLSearchParams({
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: REDIRECT_URI,
+      }).toString(),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
+      }
+    }
+
+    const { status, data: { access_token, refresh_token, expires_in } } = await axios(authOptions);
+
+    // return res.status(status).json(data);
+
+    if (status == 200) {
+      const queryParams = new URLSearchParams({
+        access_token,
+        refresh_token,
+        expires_in,
+      }).toString();
+
+      return res.redirect(`${FRONTEND_URI}?${queryParams}`);
+    } else {
+      return res.redirect('/error=invalid_token');
+    }
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.listen(8888, () => console.log(`Server started on ${PORT}`));
